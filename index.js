514d58a1c9fcb58b4331ed6e3380fcf635121f4b
aveen007 <aveen2000hussein@gmail.com>
2022-06-05 02:25:21 +0200

    using morgan

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { text } = require("body-parser");
 const express=require("express");
+const morgan=require("morgan");
 const http=require("http");
 const { hostname } = require("os");
 const hosttname ='localhost';
@@ -7,12 +8,13 @@ const port=3000;
 
 
 const app=express();
-
+app.use(morgan('dev'));
+app.use(express.static(__dirname+'/public'));
 
 app.use((req,res,next)=>{
 
 
-    console.log(req.headers);
+  //  console.log(req.headers);
     res.statusCode=200;
 
     res.setHeader('Content-Type','text/html');
