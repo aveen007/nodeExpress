d81db52a0a110c6a9d2fe179e0189d824c83121a
aveen007 <aveen2000hussein@gmail.com>
2022-06-06 19:20:24 +0200

    express router

--- index.js
+++ index.js
@@ -1,46 +1,77 @@
-const bodyParser = require('body-parser');
+const { text } = require("body-parser");
+const express=require("express");
+const morgan=require("morgan");
+const http=require("http");
+const bodyParser= require('body-parser');
+const { hostname } = require("os");
+const hosttname ='localhost';
+const port=3000;
 
+const dishRouter = require('./routes/dishRouter');
 
+app.use('/dishes', dishRouter);
+
+const app=express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 
-app.all('/dishes', (req,res,next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  next();
-});
+// app.all('/dishes',(req,res,next)=>{
 
-app.get('/dishes', (req,res,next) => {
-    res.end('Will send all the dishes to you!');
-});
+//   res.statusCode=200;
 
-app.post('/dishes', (req, res, next) => {
- res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
+//   res.setHeader('Content-Type','text/plain');
+//   // res.end("<html><body><h1> This is EXPRESS</h1></body></html>");
 
-app.put('/dishes', (req, res, next) => {
-  res.statusCode = 403;
-  res.end('PUT operation not supported on /dishes');
-});
- 
-app.delete('/dishes', (req, res, next) => {
-    res.end('Deleting all dishes');
-});
+//   next();
 
-app.get('/dishes/:dishId', (req,res,next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
-});
+// });
 
-app.post('/dishes/:dishId', (req, res, next) => {
-  res.statusCode = 403;
-  res.end('POST operation not supported on /dishes/'+ req.params.dishId);
-});
+// app.get('/dishes',(req,res,next)=>{
 
-app.put('/dishes/:dishId', (req, res, next) => {
-  res.write('Updating the dish: ' + req.params.dishId + '\n');
-  res.end('Will update the dish: ' + req.body.name + 
-        ' with details: ' + req.body.description);
-});
+//   res.end('we will return to you');
 
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});
+// }
+// );
+
+// app.put('/dishes',(req,res,next)=>{
+
+//   res.end('we will return to you'+ req.body.name );
+
+
+
+// });
+
+// app.delete('/dishes',(req,res,next)=>{
+
+//   res.end('we will delete' );
+
+
+
+// });
+// app.post('/dishes',(req,res,next)=>{
+
+//   statusCode=403;
+//   res.end('we do not support put');
+
+
+
+// });
+app.use((req,res,next)=>{
+
+
+  //  console.log(req.headers);
+    res.statusCode=200;
+
+    res.setHeader('Content-Type','text/html');
+    res.end("<html><body><h1> This is EXPRESS</h1></body></html>");
+
+}
+
+
+);
+
+const server=http.createServer(app);
+
+server.listen(port,hosttname,()=>{console.log(`server running at http://${hosttname}/${port}`);});
+
